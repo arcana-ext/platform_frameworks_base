6b94edd52c5c9dade3a10cb4de989f53a825a304
maxwen <max.weninger@gmail.com>
2022-04-02 22:06:49 +0200

    SystemUI: fix qs tiles init on split notification shade open
    thanks to 294f8c51ea6aa8c9d752ab0ac0bdeace9a5b5d9c
    mQsExpanded is no longer reflecting the value
    that should be passed to setListening which will
    combine listening && expanded to get the final
    value passing further for setListening
    
    the value to use for expanded is
    final boolean expanded = mQsExpanded || mInSplitShade
    
    so the qs tiles where not getting the correct
    value for setListening
    
    Change-Id: Iefa0c14470d82a6474f46bb854cd2bf3bdf53937
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -448,7 +448,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     public void setExpanded(boolean expanded) {
         if (DEBUG) Log.d(TAG, "setExpanded " + expanded);
         mQsExpanded = expanded;
-        mQSPanelController.setListening(mListening, mQsExpanded);
+        mQSPanelController.setListening(mListening, mQsExpanded || mInSplitShade);
         updateQsState();
     }
 
@@ -477,7 +477,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mListening = listening;
         mQSContainerImplController.setListening(listening);
         mFooter.setListening(listening);
-        mQSPanelController.setListening(mListening, mQsExpanded);
+        mQSPanelController.setListening(mListening, mQsExpanded || mInSplitShade);
     }
 
     @Override
